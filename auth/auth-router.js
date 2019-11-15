1303b1af2bbe5f046d6212176c13db3f9c02caf5
Victor Arowo <arowovici@gmail.com>
2019-11-15 09:38:31 +0100

    Added Login and Register Logic with JWT Authentication

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,44 @@
 const router = require('express').Router();
+const { getUser, postUser } = require('./auth-model');
+const generateToken = require('../utils/generateToken');
+const bcrypt = require('bcryptjs');
+router.post('/register', async (req, res) => {
+  let { username, password } = req.body;
 
-router.post('/register', (req, res) => {
-  // implement registration
+  try {
+    let exists = await getUser(username);
+    if (exists) return res.status(400).json({ error: 'User already exists' });
+
+    const hashedPassword = bcrypt.hashSync(password, 14);
+    let newUser = {
+      username,
+      password: hashedPassword
+    };
+
+    let user = await postUser(newUser);
+    const token = generateToken(user);
+    res.status(201).json({ token });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
-router.post('/login', (req, res) => {
-  // implement login
+router.post('/login', async (req, res) => {
+  let { username, password } = req.body;
+
+  try {
+    let user = await getUser(username);
+    if (!user) return res.status(400).json({ error: "User doesn't exist" });
+
+    if (bcrypt.compareSync(password, user.password)) {
+      const token = generateToken(user);
+
+      return res.status(200).json({ token });
+    }
+    return res.status(401).json({ message: 'Invalid Credentials' });
+  } catch (error) {
+    return res.status(500).json({ error });
+  }
 });
 
 module.exports = router;
